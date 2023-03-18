67e1411b4fde6dc2f682b0ee76cca11be30079ec
Relic <RobertSmithTheFourth@Outlook.com>
2023-03-18 08:18:46 -0400

    trying a fix for sqlalchemy database uri found on slack

--- app.py
+++ app.py
@@ -15,10 +15,16 @@ CURR_USER_KEY = "curr_user"
 
 app.config['SECRET_KEY'] = "pokemonmeandyou"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///pokedex')
-    # .replace('://', 'ql://', 1)
-    )
+
+uri = os.environ.get('DATABASE_URL', 'postgresql:///pokedex')
+if uri.startswith('postgres://'):
+	uri = uri.replace('postgres://', 'postgresql://', 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+# app.config['SQLALCHEMY_DATABASE_URI'] = (
+#     os.environ.get('DATABASE_URL', 'postgresql:///pokedex')
+#     .replace('://', 'ql://', 1)
+#     )
 
 connect_db(app)
 
